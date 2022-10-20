d720663192ec070c40c8fcaeabc91f1f4f52945c
Melikidis <melikalai232@gmail.com>
2022-10-20 12:29:44 +0300

    added value objects

--- src/MatchOdds.Infrastructure/MatchOddsContext.cs
+++ src/MatchOdds.Infrastructure/MatchOddsContext.cs
@@ -1,5 +1,6 @@
 ﻿using MatchOdds.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace MatchOdds.Infrastructure
 {
@@ -16,15 +17,33 @@ namespace MatchOdds.Infrastructure
         public DbSet<Match> Match { get; set; }
         public DbSet<Odd> Odd { get; set; }
 
+        //https://blog.devgenius.io/3-different-ways-to-implement-value-object-in-csharp-10-d8f43e1fa4dc
+        //protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        //{
+        //    configurationBuilder.Properties<Match>().HaveConversion<OddConverter>();
+        //}
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Match>()
-                .HasIndex(p => new { p.MatchDate, p.TeamA }).IsUnique();
-            modelBuilder.Entity<Match>()
-                .HasIndex(p => new { p.MatchDate, p.TeamB }).IsUnique();
-            modelBuilder.Entity<Match>()
-                .HasIndex(p => new { p.MatchDate, p.TeamA, p.TeamB }).IsUnique();
+            modelBuilder.Entity<Match>(e =>
+            {               
+                e.ToTable("Match").HasKey(x => x.ID);
+                e.HasIndex(p => new { p.MatchDate, p.TeamA }).IsUnique();
+                e.HasIndex(p => new { p.MatchDate, p.TeamB }).IsUnique();
+                e.HasIndex(p => new { p.MatchDate, p.TeamA, p.TeamB }).IsUnique();
+                e.Property(x => x.Description).HasMaxLength(255).HasDefaultValue(string.Empty);
+                e.Property(x => x.TeamA).HasMaxLength(255).HasDefaultValue(string.Empty);
+                e.Property(x => x.TeamB).HasMaxLength(255).HasDefaultValue(string.Empty);
+                e.Property(x => x.Sport);
+            });
+
+            modelBuilder.Entity<Odd>(e =>
+            {
+                e.ToTable("Odd").HasKey(x => x.ID);
+                e.Property(x => x.Specifier).HasMaxLength(255).HasDefaultValue(string.Empty);
+                e.Property(x => x.MatchOdd);
+            });
         }
     }
 }
